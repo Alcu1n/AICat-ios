ae5badb564095d86f498aae702c8f470bee322b3
Alcu1n <41295967+Alcu1n@users.noreply.github.com>
2023-04-26 03:33:59 +0800

    Update Credential.swift
    .

--- AIChatty/Credential.swift
+++ AIChatty/Credential.swift
@@ -4,9 +4,9 @@
 //  Created by Apple on 2023/4/20.
 //
 // for App Crash Tracking
-let appCenterSecretKey = "{6b83e3c9-4fbf-4d2a-a39e-56741e75b3d6}"
+let appCenterSecretKey = ""
 // for in app purchase
-let appHudKey = "{app_Q3RyXHtbrPvvPmw8izPGGfgqFewP1T}"
+let appHudKey = ""
 // open ai
-let openAIKey = "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"
-let proxyAPIHost = "https://service-jeuvyl6m-1300366651.usw.apigw.tencentcs.com"
+let openAIKey = ""
+let proxyAPIHost = ""
